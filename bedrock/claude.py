3e7441586f43029bcd227f979be0393c62affcd7
liyxmse <liyxmse@amazon.com>
2024-01-22 11:19:43 +0800

    add guide for read timeout configuration

--- bedrock/claude.py
+++ bedrock/claude.py
@@ -1,8 +1,15 @@
 import boto3
 import json
+from botocore.config import Config
+
+# 设置读取超时时间 https://guide.aws.dev/articles/AR4TEI8aAsT0qtqrbfnD5NPg
+config = Config(read_timeout=1000)
+
 
 # claude is provided via bedrock in us-west-2, us-east-1 currently
-bedrock = boto3.client(service_name="bedrock-runtime", region_name='us-west-2')
+bedrock = boto3.client(service_name="bedrock-runtime",
+                       region_name='us-west-2',
+                       config=config)
 body = json.dumps(
     {
         "prompt": "\n\nHuman: Write a very short essay about space travel to Mars\n\nAssistant:",
